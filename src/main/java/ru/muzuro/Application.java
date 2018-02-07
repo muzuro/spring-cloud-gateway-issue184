3ee49c00f0df0b38777777f832a46f8c6c4858da
mzr <zufarm@gmail.com>
2018-02-07 23:01:14 +0300

    add manual static route

--- src/main/java/ru/muzuro/Application.java
+++ src/main/java/ru/muzuro/Application.java
@@ -6,6 +6,11 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.Routes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.resources;
 
 @Configuration
 @SpringBootApplication
@@ -17,6 +22,11 @@ public class Application {
                 .route("userapi")
                 .uri("http://httpbin.org:80")
                 .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> monoRouterFunction() {
+        return resources("/static/**", new ClassPathResource("static/"));
     }
 
     public static void main(String[] args) {
