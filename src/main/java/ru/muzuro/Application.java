58b66f5030fa983dc1e83064151fe8cd3dca5b94
Zufar Muhamadeev <zufarm@gmail.com>
2018-02-08 15:17:29 +0300

    update api with using new gateway RouteLocatorBuilder

--- src/main/java/ru/muzuro/Application.java
+++ src/main/java/ru/muzuro/Application.java
@@ -3,30 +3,19 @@ package ru.muzuro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.Routes;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
-
-import static org.springframework.web.reactive.function.server.RouterFunctions.resources;
 
 @Configuration
 @SpringBootApplication
 public class Application {
 
     @Bean
-    public RouteLocator routes() {
-        return Routes.locator()
-                .route("userapi")
-                .uri("http://httpbin.org:80")
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/get").uri("http://httpbin.org:80"))
                 .build();
-    }
-
-    @Bean
-    public RouterFunction<ServerResponse> monoRouterFunction() {
-        return resources("/static/**", new ClassPathResource("static/"));
     }
 
     public static void main(String[] args) {
